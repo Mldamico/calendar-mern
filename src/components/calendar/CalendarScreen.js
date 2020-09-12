d66a8c9a890389cbf46cd7c0af5038a26fdd8580
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-12 20:38:10 -0300

    Loginscreen and navbar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Navbar } from '../ui/Navbar';
 
 export const CalendarScreen = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
